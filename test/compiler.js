d2ff9e7902ec60cb0484f6c48af961b0c20bf61e
Luke Horvat <lukehorvat@gmail.com>
2021-06-17 17:58:06 +0200

    Improve fixture structure

--- test/compiler.js
+++ test/compiler.js
@@ -1,11 +1,48 @@
 const path = require('path');
+const fs = require('fs');
 const webpack = require('webpack');
-const { createFsFromVolume, Volume } = require('memfs');
+const { ufs } = require('unionfs');
+const { Volume } = require('memfs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackEntrypointsPlugin = require('../..');
 
-module.exports = (config) => {
-  const compiler = webpack(config);
-  compiler.outputFileSystem = createFsFromVolume(new Volume());
-  compiler.outputFileSystem.join = path.join.bind(path);
+module.exports = (fixture) => {
+  const fixtureDir = path.join(__dirname, 'fixtures', fixture);
+  const fixtureConfig = JSON.parse(
+    fs.readFileSync(path.join(fixtureDir, 'fixture.json'))
+  );
+  const compiler = webpack({
+    mode: 'development',
+    context: fixtureDir,
+    entry: {
+      app: './app.js',
+    },
+    output: {
+      filename: path.join('assets', '[name].js'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: fixtureConfig.inject,
+        template: './template.ejs',
+        publicPath: 'https://example.com',
+      }),
+
+      // new HtmlWebpackEntrypointsPlugin(),
+    ],
+  });
+
+  // Create a filesystem that is a union of the real disk filesystem and an in-memory volume.
+  // This is done for two reasons:
+  // 1. Webpack will emit the build to an in-memory filesystem location when running the tests.
+  // 2. Unimportant files that are needed for the tests don't have to exist on disk.
+  compiler.outputFileSystem = compiler.inputFileSystem = ufs.use(fs).use(
+    Volume.fromJSON(
+      {
+        './app.js': '',
+      },
+      fixtureDir
+    )
+  );
 
   return new Promise((resolve, reject) => {
     compiler.run((err, stats) => {
--- test/plugin.test.js
+++ test/plugin.test.js
@@ -2,28 +2,22 @@ const path = require('path');
 const fs = require('fs');
 const util = require('util');
 const compiler = require('./compiler');
-const webpackConfig = require('./fixtures/webpack.config');
 
 it('is a test', async () => {
-  const actualHtml = await readActualHtml(webpackConfig);
-  const expectedHtml = await readExpectedHtml();
-  expect(actualHtml).toBe(expectedHtml);
+  await runFixture('single-entrypoint');
 });
 
-async function readExpectedHtml() {
-  const html = await util.promisify(fs.readFile)(
-    path.join(__dirname, 'fixtures', 'expected.html'),
-    'utf-8'
-  );
-  return html.trim();
-}
-
-async function readActualHtml(config) {
-  const stats = await compiler(config);
+async function runFixture(fixture) {
+  const stats = await compiler(fixture);
   const webpackFs = stats.compilation.compiler.outputFileSystem;
-  const html = webpackFs.readFileSync(
+  const actualHtml = webpackFs.readFileSync(
     path.join(stats.compilation.compiler.outputPath, 'index.html'),
     'utf-8'
   );
-  return html.trim();
+  const expectedHtml = await util.promisify(fs.readFile)(
+    path.join(__dirname, 'fixtures', fixture, 'expected.html'),
+    'utf-8'
+  );
+
+  expect(actualHtml.trim()).toBe(expectedHtml.trim());
 }
